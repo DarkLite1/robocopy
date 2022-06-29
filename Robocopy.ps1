4f3697acc298a579ff436ccbc9d3dbc5187cfb81
DarkLite1 <BrechtGijbels@gmail.com>
2022-06-29 14:43:29 +0200

    add ConvertFrom-RobocopyExitCodeHC

--- Robocopy.ps1
+++ Robocopy.ps1
@@ -1,3 +1,7 @@
+#Requires -Version 5.1
+#Requires -Modules ImportExcel
+#Requires -Modules Toolbox.HTML, Toolbox.Remoting, Toolbox.EventLog
+
 <#
     .SYNOPSIS
         Copy files and folders with Robocopy.exe
@@ -47,6 +51,90 @@ Param(
 )
 
 Begin {
+    Function ConvertFrom-RobocopyExitCodeHC {
+        <#
+        .SYNOPSIS
+            Convert exit codes of Robocopy.exe.
+    
+        .DESCRIPTION
+            Convert exit codes of Robocopy.exe to readable formats.
+    
+        .EXAMPLE
+            Robocopy.exe $Source $Target $RobocopySwitches
+            ConvertFrom-RobocopyExitCodeHC -ExitCode $LASTEXITCODE
+            'COPY'
+    
+        .NOTES
+            $LASTEXITCODE of Robocopy.exe
+    
+            Hex Bit Value Decimal Value Meaning If Set
+            0×10 16 Serious error. Robocopy did not copy any files. This is either 
+                 a usage error or an error due to insufficient access privileges on 
+                 the source or destination directories.
+            0×08 8 Some files or directories could not be copied (copy errors   
+                 occurred and the retry limit was exceeded). Check these errors 
+                 further.
+            0×04 4 Some Mismatched files or directories were detected. Examine the 
+                 output log. Housekeeping is probably necessary.
+            0×02 2 Some Extra files or directories were detected. Examine the 
+                 output log. Some housekeeping may be needed.
+            0×01 1 One or more files were copied successfully (that is, new files 
+                 have arrived).
+            0×00 0 No errors occurred, and no copying was done. The source and 
+                 destination directory trees are completely synchronized.
+    
+            (https://support.microsoft.com/en-us/kb/954404?wa=wsignin1.0)
+    
+            0	No files were copied. No failure was encountered. No files were 
+                mismatched. The files already exist in the destination directory; 
+                therefore, the copy operation was skipped.
+            1	All files were copied successfully.
+            2	There are some additional files in the destination directory that 
+                are not present in the source directory. No files were copied.
+            3	Some files were copied. Additional files were present. No failure 
+                was encountered.
+            5	Some files were copied. Some files were mismatched. No failure was 
+                encountered.
+            6	Additional files and mismatched files exist. No files were copied 
+                and no failures were encountered. This means that the files already 
+                exist in the destination directory.
+            7	Files were copied, a file mismatch was present, and additional  
+                files were present.
+            8	Several files did not copy.
+            
+            * Note Any value greater than 8 indicates that there was at least one 
+            failure during the copy operation.
+            #>
+    
+        Param (
+            [int]$ExitCode
+        )
+    
+        Process {
+            Switch ($ExitCode) {
+                0 { $Message = 'NO CHANGE'; break }
+                1 { $Message = 'COPY'; break }
+                2 { $Message = 'EXTRA'; break }
+                3 { $Message = 'EXTRA + COPY'; break }
+                4 { $Message = 'MISMATCH'; break }
+                5 { $Message = 'MISMATCH + COPY'; break }
+                6 { $Message = 'MISMATCH + EXTRA'; break }
+                7 { $Message = 'MISMATCH + EXTRA + COPY'; break }
+                8 { $Message = 'FAIL'; break }
+                9 { $Message = 'FAIL + COPY'; break }
+                10 { $Message = 'FAIL + EXTRA'; break }
+                11 { $Message = 'FAIL + EXTRA + COPY'; break }
+                12 { $Message = 'FAIL + MISMATCH'; break }
+                13 { $Message = 'FAIL + MISMATCH + COPY'; break }
+                14 { $Message = 'FAIL + MISMATCH + EXTRA'; break }
+                15 { $Message = 'FAIL + MISMATCH + EXTRA + COPY'; break }
+                16 { $Message = 'FATAL ERROR'; break }
+                default { 'UNKNOWN' }
+            }
+            return $Message
+        }
+    }
+    
     $scriptBlock = {
         Param (
             [Parameter(Mandatory)]
